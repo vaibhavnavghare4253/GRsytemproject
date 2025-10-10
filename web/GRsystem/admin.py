d9c00de1fae7f4b38af3dd18d0616b32d14bacb9
Navghare__vaibhav <75133602+vaibhavnavghare4253@users.noreply.github.com>
2025-10-10 16:12:44 +0530

    Almighty push

--- web/GRsystem/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile,Complaint,Grievance
-
-class CAdmin(admin.ModelAdmin):
-    list_display = ('user','Subject','Type_of_complaint','Description','Time','status')
-
-admin.site.register(Profile)
-admin.site.register(Complaint,CAdmin)
-admin.site.register(Grievance)
